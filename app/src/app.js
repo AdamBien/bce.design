8e25771be8aafb1035a9a336114aad9cfb9628b0
Adam Bien <abien@adam-bien.com>
2021-04-06 17:56:00 +0200

    router initialized

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,12 @@
-console.log("Hello Web Standards");
+import { Router } from "./libs/vaadin-router.js";
+import './add/boundary/Add.js';
+
+console.log("router initialization");
 
 
-
+const outlet = document.querySelector('.view');
+const router = new Router(outlet);
+router.setRoutes([
+  {path: '/',     component: 'b-list'},
+  {path: '/add',  component: 'b-add'}
+]);
