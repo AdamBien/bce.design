9c5f87d323d707c67326dae5618f827a1fd86890
Adam Bien <abien@adam-bien.com>
2021-04-06 19:03:50 +0200

    keyup listener registered

--- app/src/add/boundary/Add.js
+++ app/src/add/boundary/Add.js
@@ -1,17 +1,18 @@
 import BElement from "../../Belement.js";
 import { html } from "../../libs/lit-html.js";
 import { bookmarkUpdated } from "../control/AddControl.js";
-
+import '../../preview/boundary/Preview.js';
 class Add extends BElement{
     
     view() {
         return html`
+        <b-preview></b-preview>
         <form>
             <label class="label">Label:
-                <input ?disabled=${status} class="input is-primary" required name="label" placeholder="label" @change=${e=>this.onUserInput(e)} >
+                <input ?disabled=${status} class="input is-primary" required name="label" placeholder="label" @keyup=${e=>this.onUserInput(e)} >
             </label>
             <label class="label">Link:
-                <input ?disabled=${status} class="input is-primary" required name="link" placeholder="link" @change=${e=>this.onUserInput(e)} >
+                <input ?disabled=${status} class="input is-primary" required name="link" placeholder="link" @keyup=${e=>this.onUserInput(e)} >
             </label>
         </form>
         `;
