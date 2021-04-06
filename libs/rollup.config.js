28d573d529d9153b47715d3a3a24de1ed163cfda
Adam Bien <abien@adam-bien.com>
2021-04-06 14:53:20 +0200

    vaadin router installed

--- libs/rollup.config.js
+++ libs/rollup.config.js
@@ -4,6 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default [{
   input: [
     './node_modules/lit-html/lit-html.js',
+    './node_modules/@vaadin/router/dist/vaadin-router.js',
     './node_modules/@reduxjs/toolkit/dist/redux-toolkit.esm.js'
   ],
     output: { dir: "../app/src/libs", format: "esm" },
