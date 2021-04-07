78837ddad00a001b1712fdd803c5c17ecc543230
Adam Bien <abien@adam-bien.com>
2021-04-07 09:12:23 +0200

    levery change is stored to localstorage

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,13 @@
 import { Router } from "./libs/vaadin-router.js";
 import './add/boundary/Add.js';
 import './list/boundary/List.js';
+import store from "./store.js";
+import { save } from "./localstorage/control/StorageControl.js";
 
+store.subscribe(_ => { 
+    const state = store.getState();
+    save(state);
+})
 const outlet = document.querySelector('.view');
 const router = new Router(outlet);
 router.setRoutes([
