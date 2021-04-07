a059426cc9e2d5ef73c1c668c7ce8fbeaf340a5c
Adam Bien <abien@adam-bien.com>
2021-04-07 08:30:32 +0200

    newBookmark case added

--- app/src/add/entity/BookmarksReducer.js
+++ app/src/add/entity/BookmarksReducer.js
@@ -1,5 +1,5 @@
 import { createReducer } from "../../libs/redux-toolkit.esm.js"
-import { bookmarkUpdatedAction } from "../control/AddControl.js";
+import { bookmarkUpdatedAction, newBookmarkAction } from "../control/AddControl.js";
 
 const initialState = {
     list: [],
@@ -9,5 +9,7 @@ const initialState = {
 export const bookmarks = createReducer(initialState, (builder) => {
     builder.addCase(bookmarkUpdatedAction, (state, { payload: { name, value } }) => {
         state.bookmark[name] = value;
+    }).addCase(newBookmarkAction, (state, _) => {
+        state.list.concat(state.bookmark)
     });
 })
