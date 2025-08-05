9ecb65a889ce14b8964326890d59aea323eccbbe
Adam Bien <abien@adam-bien.com>
2025-08-05 07:46:46 +0200

    local storage deactivation described

--- app/src/app.js
+++ app/src/app.js
@@ -1,9 +1,18 @@
+/**
+ * Application entry point that initializes routing and state persistence.
+ * Configures client-side routing and sets up automatic localStorage synchronization
+ * for Redux store updates.
+ * To deactivate localStorage persistence, comment out or remove the store.subscribe() block.
+ */
 import { Router } from "./libs/router.js";
 import './bookmarks/boundary/Bookmarks.js';
 import './bookmarks/boundary/List.js';
 import store from "./store.js";
 import { save } from "./localstorage/control/StorageControl.js";
 
+/**
+ * To deactivate localStorage persistence, comment out or remove the store.subscribe() block below.
+ */
 store.subscribe(_ => { 
     const state = store.getState();
     save(state);
