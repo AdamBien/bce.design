26b6901bf8798fd84bc64abb3e660afc4c80a3d5
Adam Bien <abien@adam-bien.com>
2025-08-05 07:43:12 +0200

    localStorage described

--- app/src/store.js
+++ app/src/store.js
@@ -1,3 +1,9 @@
+/**
+ * Redux store configuration with automatic localStorage persistence.
+ * The entire application state is persisted to localStorage on every update,
+ * enabling state recovery across browser sessions.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage}
+ */
 import { configureStore } from "./libs/redux-toolkit.modern.js";
 import { load } from "./localstorage/control/StorageControl.js";
 import { bookmarks } from "./bookmarks/entity/BookmarksReducer.js"
