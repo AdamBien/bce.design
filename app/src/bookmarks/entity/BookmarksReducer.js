062006a86981c40e2bad1894beac8ef257182361
Adam Bien <abien@adam-bien.com>
2025-08-06 07:39:15 +0200

    bookmarks reducer described

--- app/src/bookmarks/entity/BookmarksReducer.js
+++ app/src/bookmarks/entity/BookmarksReducer.js
@@ -10,6 +10,14 @@ const removeBookmarkWithId = (list, id) => {
     return list.filter(bookmark => bookmark.id !== id);
 }
 
+/**
+ * Redux reducer managing bookmark state transitions.
+ * 
+ * Maintains a list of bookmarks and a temporal cache for bookmark creation.
+ * Updates flow through the cached bookmark object before being committed
+ * to the list upon save. Supports incremental field updates, creation,
+ * and deletion operations.
+ */
 export const bookmarks = createReducer(initialState, (builder) => {
     builder.addCase(bookmarkUpdatedAction, (state, { payload: { name, value } }) => {
         state.bookmark[name] = value;
